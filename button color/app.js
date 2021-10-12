62d81673b03bfdd8962a9abb9375a751cd9c2825
shar <81372629+shar27@users.noreply.github.com>
2021-10-12 22:01:08 +0200

    Add files via upload

---
+++ button color/app.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+
+
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {
+if(btn.classList.toggle("mystyle"));
+btn.classList.toggle("nextstyle");}
+btn.classList.toggle("mystyle");
